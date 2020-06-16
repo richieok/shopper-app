c93f1725dbce93026952b282e31ee2a1213c13fe
richieok <richie.okoro@gmail.com>
2020-06-16 10:11:29 +0100

    test is now shop

---
+++ src/routes/shop/[...slug].svelte
@@ -0,0 +1,16 @@
+<script context="module">
+    export async function preload({ params }){
+        let [ slug, item ] = params.slug;
+        return { slug, item };
+    }
+</script>
+
+<script>
+    export let slug;
+    export let item;
+    let brands = ['nike', 'adidas'];
+    let categories = ['shoes', 'belts'];
+    // console.log(`${slug}, ${item}`);
+
+</script>
+
---
+++ src/routes/shop/index.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+    export function preload({ params, query }){
+        return this.fetch(`shop.json`).then( r => r.json()).then( data => {
+            return { data };
+        });
+    }
+</script>
+
+<script>
+    export let data;
+</script>
+
+<svelte:head>
+    <title>Shop</title>
+</svelte:head>
